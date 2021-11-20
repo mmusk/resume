6906ba24c724af37761ce3499fc494744a87365d
Muzakkir <mdmuzakkir296@gmail.com>
2021-11-20 21:44:12 +0530

    connected: website to mail

--- mail.php
+++ mail.php
@@ -2,11 +2,12 @@
 //get data from form  
 $name = $_POST['name'];
 $email= $_POST['email'];
+$project= $_POST['message'];
 $message= $_POST['message'];
 $to = "mdmuzakkir296@gmail.com";
 $subject = "Mail From website";
-$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
-$headers = "From: noreply@yoursite.com" . "\r\n" .
+$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n  Project = ". $message . "\r\n Message =" . $message;
+$headers = "From: noreply@https://mmusk.github.io/Muzakkir__Portfolio/#" . "\r\n" .
 "CC: somebodyelse@example.com";
 if($email!=NULL){
     mail($to,$subject,$txt,$headers);
